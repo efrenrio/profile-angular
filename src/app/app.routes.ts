1c3f086d16c8c98e6b09bd5c845eab499034d9ab
efrenrio <efren.rio@gmail.com>
2026-07-26 04:11:55 +0800

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 
-import { ActivityComponent } from './activity.component';
-import { OverviewComponent } from './overview.component';
-import { ProfileComponent } from './profile.component';
-import { SettingsComponent } from './settings.component';
-import { SupportComponent } from './support.component';
+import { ActivityComponent } from './activity/activity.component';
+import { OverviewComponent } from './overview/overview.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SupportComponent } from './support/support.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'overview', pathMatch: 'full' },
