5c697c3b539464d534a5ca4f176f1c732aadb8bf
efrenrio <efren.rio@gmail.com>
2026-07-26 08:22:12 +0800

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 
 import { ActivityComponent } from './activity/activity.component';
 import { OverviewComponent } from './overview/overview.component';
-import { ProfileComponent } from './profile/profile.component';
+import { WorkExperiencesComponent } from './work-experiences/work-experiences.component';
 import { SettingsComponent } from './settings/settings.component';
 import { SupportComponent } from './support/support.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'overview', pathMatch: 'full' },
   { path: 'overview', component: OverviewComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'work-experiences', component: WorkExperiencesComponent },
   { path: 'activity', component: ActivityComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'support', component: SupportComponent },
---
+++ src/app/work-experiences/work-experiences.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-experiences',
+  templateUrl: './work-experiences.component.html',
+  styleUrl: './work-experiences.component.scss'
+})
+export class WorkExperiencesComponent {}
